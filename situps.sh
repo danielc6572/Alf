313a059cd92a3224ed61ebf000470591ebc66df9
Daniel C <danielc6572@gmail.com>
2022-12-15 15:10:51 -0600

    Yay

--- situps.sh
+++ situps.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
+situps=1
 
-for situp in {1..100} 
-do  
-  if [[ $situp -le 10 ]]; then
-    continue
-    echo "Do you think this is a game?"
-  if [[ $situp -le 20 ]]; then
-    continue
-    echo "Keep going cupcake!"
-  if [[ $situp == 100 ]]; then 
-    continue
-  fi
-  echo "Good job on doing $situp situps"
-  sleep 1
+while [[ $situps -le 100 ]]
+do
+  read -p "Situp $situps: Do you think this is a game!?"
+  (( situps ++ ))
 done
-
- 
+echo "Good job ... tomorrow do $situps*2 situps"
